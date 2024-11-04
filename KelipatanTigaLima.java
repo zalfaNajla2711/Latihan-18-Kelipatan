49e63ef7dce412446672693180565a2bd70a8608
zalfaNajla2711 <zalfadhianajla@gmail.com>
2024-11-04 13:42:18 +0700

    Add files via upload

---
+++ KelipatanTigaLima.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package kelipatantigalima;
+
+/**
+ *
+ * @author hp5cd
+ * NAMA: Zalfa Dhia Najla
+ * KELAS: PBO12
+ * NIM: 23176008
+ * PRODI: Sistem Informasi
+ * SEMESTER: 3
+ * Deskripsi Program: Program ini berisi program untuk menampilkan
+ * program kelipatan
+ * 
+ */
+public class KelipatanTigaLima {
+    public static void main(String[] args) {
+        double kelipatan = 3.5; // Nilai awal
+        double batas = 35.0;    // Batas nilai
+
+        System.out.println("Bilangan kelipatan 3.5 dari 3.5 sampai 35:");
+
+        // Loop untuk mencetak kelipatan 3.5
+        while (kelipatan <= batas) {
+            System.out.printf("%.1f ", kelipatan);  // Format satu angka desimal
+            kelipatan += 3.5;                       // Menambahkan 3.5 ke nilai saat ini
+        }
+    }
+}
